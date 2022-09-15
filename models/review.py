b3594c3497670e4f765a93fa5210357c07e0e50f
Diana Gatwiri <99605339+Dianedylan@users.noreply.github.com>
2022-09-15 22:35:48 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""This is the review class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from os import getenv
+"""
+review.py
+Review Class
+"""
+
+from models.base_model import BaseModel
 
 
-class Review(BaseModel, Base):
-    """This is the class for Review
-    Attributes:
-        __tablename__: table reviews
-        place_id: place id
-        user_id: user id
-        text: review description
+class Review(BaseModel):
     """
-    __tablename__ = "reviews"
-    text = Column("text", String(1024), nullable=False)
-    place_id = Column("place_id", String(60),
-                      ForeignKey("places.id"), nullable=False)
-    user_id = Column("user_id", String(60),
-                     ForeignKey("users.id"), nullable=False)
+    This a child class of BaseModel
+    It'll be a review for HBnB
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
