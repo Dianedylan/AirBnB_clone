bb9dcc7e7467ae9a7149ab4629a015826f338138
Diana Gatwiri <99605339+Dianedylan@users.noreply.github.com>
2022-09-15 22:34:50 +0300

    updated state class

--- models/state.py
+++ models/state.py
@@ -1,34 +1,15 @@
 #!/usr/bin/python3
-"""This is the state class"""
-import models
-from models.base_model import BaseModel, Base
-from models.city import City
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from os import getenv
+"""
+state.py
+State Class
+"""
+
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
+class State(BaseModel):
     """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship('City', backref='state')
-    else:
-        @property
-        def cities(self):
-            """
-            Returns the list of City instances where
-            state_id equals the current State.id
-            """
-            city_list = []
-            city_dict = models.storage.all(City)
-            for key, value in city_dict.items():
-                if city_dict[key].state_id == self.id:
-                    city_list.append(city_dict[key])
-            return city_list
+    This a child class of BaseModel
+    It'll define States for HBnB
+    """
+    name = ""
