b17f8b9d4a5ef5b469ea43d3fd2bbb466829652b
Diana Gatwiri <99605339+Dianedylan@users.noreply.github.com>
2022-09-15 22:38:51 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""This is the amenity class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from os import getenv
+"""
+amenity.py
+Amenity Class
+"""
+
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
-    """This is the class for Amenity
-    Attributes:
-        __tablename__: table Amenities
-        name: input name
+class Amenity(BaseModel):
     """
-    __tablename__ = "amenities"
-    name = Column("name", String(128), nullable=False)
+    This a child class of BaseModel
+    It'll define the Amenities for HBnB
+    """
+    name = ""
